91909434c4f8a21747e385136731c6a0a9cb8aad
LSKY88 <from.russia@hotmail.com>
2022-06-13 23:21:26 +0500

    Swiper added

--- script.js
+++ script.js
@@ -122,4 +122,44 @@ const popup = () => {
 }
 popup();
 
+// Swiper
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    speed: 800,
+    loop: true,
+    grabCursor: true,
+    lazy: true,
+    autoHeight: true,
+    effect: 'creative',
+
+    creativeEffect: {
+        prev: {
+            shadow: true,
+            translate: ["-20%", 0, -1],
+        },
+        next: {
+            translate: ["100%", 0, 0],
+        },
+    },
+
+
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    },
+});
+swiper()
+
 
